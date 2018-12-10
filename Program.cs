dcd4c3617e8eef7544e2f157320fb25a0710565a
botura <botura@gmail.com>
2018-12-10 02:42:55 -0200

    Mais musicas

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace myApp
 {
@@ -19,7 +20,14 @@ namespace myApp
         clArquivo arq = new clArquivo();
         arq.conteudo = await response.Content.ReadAsByteArrayAsync();
         arq.nome = response.Content.Headers.ContentDisposition.FileName.Trim('"');
-        arq.nome = arq.nome.Replace("/","_");
+
+        arq.nome = Regex.Replace(arq.nome, @"<[^>]+>|&nbsp;", "").Trim();
+        arq.nome = Regex.Replace(arq.nome, @"[^\u0020-\u007F]", String.Empty);
+
+        arq.nome = arq.nome.Replace("/", "_");
+        arq.nome = arq.nome.Replace("?", "_");
+        arq.nome = arq.nome.Replace(" ", "_");
+        // Console.WriteLine(arq.nome);
         return arq;
       }
       // else Console.WriteLine("Não existe");
@@ -28,25 +36,25 @@ namespace myApp
 
     static async Task RunAsync()
     {
-      try
+      for (int i = 4400; i <= 9100; i++)
       {
-        for (int i = 312; i <= 9100; i++)
+        try
         {
           clArquivo arquivo = new clArquivo();
           arquivo = await GetMusicaAsync($"http://www.cante.com.br/baixar.php?id={i}");
           if (arquivo != null)
           {
             // salvar o arquivo
-            Console.WriteLine($"{i}: Salvando {arquivo.nome} ");
+            Console.WriteLine($"{i}: {arquivo.nome} ");
 
             File.WriteAllBytes($"musicas/{i}_{arquivo.nome}", arquivo.conteudo);
           }
           else Console.WriteLine($"{i}: não existe");
         }
-      }
-      catch (Exception e)
-      { 
-        Console.WriteLine(e.Message);
+        catch (Exception e)
+        {
+          Console.WriteLine($"{i}: ERRO - {e.Message}");
+        }
       }
     }
     static void Main(string[] args)
